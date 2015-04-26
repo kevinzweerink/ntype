44594de47606ea37bd23cdaf81893a22f142d389
Kevin Zweerink <kevin@kevinzweerink.com>
2015-04-26 18:57:40 -0400

    refactor

--- js/share.js
+++ js/share.js
@@ -2,9 +2,7 @@ generateShortUrl = function(settings) {
 	var req = new XMLHttpRequest();
 	var url =  'http://ntype.blue/' + encodeURIComponent(settings);
 	var endpoint = 'https://api-ssl.bitly.com/v3/shorten?access_token=d1bec5794ce59a96c31529a987dd0f507f23d62b&longUrl=' + url;
-
-	console.log(url, endpoint);
-
+	
 	req.open('GET', endpoint, true);
 
 	req.onload = function() {
